261c1ee74daca943dc95cb790ebe7d86504289e2
Pinkybrownie <64077283+Pinkybrownie@users.noreply.github.com>
2023-12-04 19:05:57 +0100

    Proyecto base

---
+++ Maq_expendedora/Maq_expendedora.srcs/sources_1/new/maq_exp.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04.12.2023 19:04:33
+-- Design Name: 
+-- Module Name: maq_exp - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity maq_exp is
+--  Port ( );
+end maq_exp;
+
+architecture Behavioral of maq_exp is
+
+begin
+
+
+end Behavioral;
